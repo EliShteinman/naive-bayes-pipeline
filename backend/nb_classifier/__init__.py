d148c6669f70443b59440640bd8d9d2ad9cffff9
eli_shteinman <a0533057932@gmail.com>
2025-07-22 14:20:16 +0300

    chore: remove unused modules, scripts, and data files for cleanup

--- backend/nb_classifier/__init__.py
+++
@@ -1,5 +0,0 @@
-from .classifier import ClassifierService
-from .data_handler import DataHandler
-from .logger_config import get_logger
-from .model_evaluator import ModelEvaluatorService
-from .naive_bayes_model_builder import NaiveBayesModelBuilder
